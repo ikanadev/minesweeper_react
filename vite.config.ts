f71a21f85ceba69ae4c003e7c32fcc3b719e6f35
Kevin <vargaskevv@gmail.com>
2023-03-21 17:32:52 -0400

    Add path resolvers and delete unused files from default vite template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// @ts-ignore
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			// @ts-ignore
+			"~": path.resolve(__dirname, "./src"),
+		},
+	},
+});
