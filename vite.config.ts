aca45b3cbb67b0119b3747b574b300f4c6417b7c
Kevin <vargaskevv@gmail.com>
2023-03-22 15:14:57 -0400

    Add vitest and test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -6,6 +7,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	test: {
+		globals: true,
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
