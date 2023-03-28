c2464270d056a9b5507a9a54c0648fd110058da8
Kevin <vargaskevv@gmail.com>
2023-03-28 14:42:00 -0400

    Add duration and startedAt to gameStore

--- src/state/game.ts
+++ src/state/game.ts
@@ -5,12 +5,18 @@ import { Board, Game, GameMove } from "~/minesweeper/types";
 
 interface GameState {
 	game: Game;
+	duration: number;
+	startedAt: number;
 	startGame: (mines: number) => void;
 	handleMove: (data: GameMove, board: Board) => void;
+	setDuration: (duration: number) => void;
+	setStartedAt: (start: number) => void;
 }
 
 export const useGameStore = create<GameState>()((set) => ({
 	game: newGame(0),
+	duration: 0,
+	startedAt: 0,
 	startGame: (mines) => set({ game: newGame(mines) }),
 	handleMove: (move, board) =>
 		set((state) => {
@@ -18,4 +24,6 @@ export const useGameStore = create<GameState>()((set) => ({
 				gameMove(move, draft.game, board);
 			});
 		}),
+	setDuration: (duration) => set({ duration }),
+	setStartedAt: (startedAt) => set({ startedAt }),
 }));
