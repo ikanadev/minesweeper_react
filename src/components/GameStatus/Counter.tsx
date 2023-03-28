98ef88465d2a27ce2d0fe4a279a063596b703110
Kevin <vargaskevv@gmail.com>
2023-03-28 14:42:24 -0400

    handle startedAt and duration to counter

--- src/components/GameStatus/Counter.tsx
+++ src/components/GameStatus/Counter.tsx
@@ -7,9 +7,10 @@ import { useGameStore } from "~/state";
 const Counter = () => {
 	const [start, setStart] = useState(0);
 	const [end, setEnd] = useState(0);
-	const { game } = useGameStore();
+	const { game, setDuration, setStartedAt } = useGameStore();
 
 	const intervalRef = useRef<NodeJS.Timer>();
+	const elapsed = end - start;
 
 	const stopTimer = useCallback(() => {
 		clearInterval(intervalRef.current);
@@ -29,6 +30,7 @@ const Counter = () => {
 			setEnd(0);
 		}
 		if (game.status === Status.Started) {
+			setStartedAt(Date.now());
 			setStart(window.performance.now());
 			setEnd(window.performance.now());
 			intervalRef.current = setInterval(() => {
@@ -37,7 +39,12 @@ const Counter = () => {
 		}
 	}, [game.status, stopTimer]);
 
-	const elapsed = end - start;
+	useEffect(() => {
+		if (game.status === Status.Win || game.status === Status.Lose) {
+			setDuration(elapsed);
+		}
+	}, [elapsed, game.status]);
+
 	return (
 		<p>
 			<Timer className="w-6 h-6 inline-block mr-1 mb-1" />
